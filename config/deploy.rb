1c787bd80ac2e59617566e2594fc5d1ef733c65d
Matthew Whittaker <mwhitt89@gmail.com>
2008-09-24 20:25:42 -0700

    changed deploy and database configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,55 @@
-# ####################
-# base config
-# ####################
+#############################################################
+#  Application
+#############################################################
+ 
 set :application, "cspot"
 set :deploy_to, "/var/www/#{application}"
-
-# ####################
-# code repository
-# ####################
+ 
+#############################################################
+#  Settings
+#############################################################
+ 
 default_run_options[:pty] = true
-set :scm, :git
-set :repository,  "git@github.com:mwhitt/copyrightspot.git"
-set :branch, "master"
-set :scm_passphrase, "qwerty89"
-set :deploy_via, :remote_cache
-
-# ####################
-# account management
-# ####################
+ssh_options[:forward_agent] = true
+set :use_sudo, true
+set :scm_verbose, true 
+ 
+#############################################################
+#  Servers
+#############################################################
+ 
 set :user, "matt"
 ssh_options[:port] = 420
-
-# ####################
-# server config
-# ####################
-role :app, "copyrightspot.com"
-role :web, "copyrightspot.com"
-role :db,  "copyrightspot.com", :primary => true
-
+set :domain, "copyrightspot.com"
+server domain, :app, :web
+role :db, domain, :primary => true
+ 
+#############################################################
+#  Git
+#############################################################
+ 
+set :scm, :git
+set :branch, "master"
+set :scm_user, 'matt'
+set :scm_passphrase, "qwerty89"
+set :repository, "git@github.com:mwhitt/copyrightspot.git"
+set :deploy_via, :remote_cache
+ 
+#############################################################
+#  Passenger
+#############################################################
+ 
 namespace :deploy do
+  
+  # Restart passenger on deploy
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
- 
+  
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
+  
 end
