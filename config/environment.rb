b0b498cb15f6560d1f8c46b963338f980a03ecd9
Matthew Whittaker <mwhitt89@gmail.com>
2008-09-28 22:44:41 -0700

    froze asciify gem

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
    config.gem 'tidy'
    config.gem 'hpricot'
    config.gem 'mechanize'
+   config.gem 'asciify'
    config.gem 'amatch'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
